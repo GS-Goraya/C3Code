0959dc049908ff6d19035e764ba4adf73c9d25cb
Gavy <Gavy@DESKTOP-HH7R25Q>
2018-11-26 22:28:38 +0530

    First Commit

---
+++ jlcapp1/src/main/java/com/jlcindia/hello/Test.java
@@ -0,0 +1,17 @@
+package com.jlcindia.hello;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Maven Core java First project started");
+	System.out.println("Commandline args");
+	for(String str:args)
+	{
+		System.out.println(str);
+		
+	}
+	System.out.println("=--------------=");
+	}
+
+}
